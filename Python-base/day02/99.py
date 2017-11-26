3771cf04e9c2073f77b62795a2dfbfff42f37a2c
Duan Guanhua <1149660164@qq.com>
2017-11-26 20:08:39 +0800

    Python基础

---
+++ Python-base/day02/99.py
@@ -0,0 +1,10 @@
+i = 1
+while i <= 9:
+    j = 1
+    while j <= i:
+        print("%d * %d = %d \t" % (i, j, i * j), end="")
+
+        j += 1
+    print('')
+    i += 1
+print('helle world ')
---
+++ Python-base/day02/for.py
@@ -0,0 +1,4 @@
+for i in '7891561899':
+    print(i)
+print(len('djakaoo'))
+print(len("ddddddd"))
